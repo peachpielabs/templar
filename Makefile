c1bbc521020e31bb4ddddfa5c4c1083c2dba0c10
a12ox <133522977+a12ox@users.noreply.github.com>
2023-05-17 22:47:46 -0600

    Initialize sentry exception tracking

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
 #!/usr/bin/make
 
 CURRENT_GIT_HASH := $(shell git rev-parse --verify HEAD)
+DSN := ${SENTRY_DSN}
+
 .PHONY: build
 build:
-	go build -a -o ./dist/gitformer -ldflags="-X github.com/peachpielabs/gitformer/cmd/gitformer.version=$(CURRENT_GIT_HASH)"
+	go build -a -o ./dist/gitformer \
+	-ldflags="-X github.com/peachpielabs/gitformer/cmd/gitformer.version=$(CURRENT_GIT_HASH) \
+			  -X main.version=$(CURRENT_GIT_HASH) \
+			  -X main.buildTime=$(shell date +%Y-%m-%dT%H:%M:%S%z) \
+			  -X main.dsn=$(DSN) \
+			  -X main.environment=development"
 
 .PHONY: test
 test:
--- main.go
+++ main.go
@@ -4,8 +4,49 @@ Copyright (c) 2023 Peach Pie Labs, LLC.
 
 package main
 
-import "github.com/peachpielabs/gitformer/cmd/gitformer"
+import (
+	"log"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/peachpielabs/gitformer/cmd/gitformer"
+)
+
+var version = "No release version provided"
+var dsn = ""
+var environment = ""
+var debug = false
 
 func main() {
+	if dsn == "" {
+		log.Fatal("DSN was not set during the build process.")
+	}
+	if environment == "" {
+		log.Fatal("environment was not set during the build process.")
+	}
+	if environment == "development" {
+		debug = true
+	}
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: dsn,
+		// Set TracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production,
+		TracesSampleRate: 1.0,
+		AttachStacktrace: true,
+		Debug:            debug,
+		SampleRate:       1.0,
+		Environment:      environment,
+		Release:          version,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
+
+	// sentry.CaptureMessage("It works!")
+	defer sentry.Recover()
+
 	gitformer.Execute()
 }
